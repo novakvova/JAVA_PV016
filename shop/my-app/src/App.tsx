7c6943f337de73072655b7f2b761b8e56f296b5b
novakvova <novakvova@gmail>
2023-02-15 20:02:22 +0200

    Add cors config

--- shop/my-app/src/App.tsx
+++ shop/my-app/src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
+
+  useEffect(() => {
+    console.log("use effect App");
+    axios.get("http://localhost:8083/api/categories")
+      .then(resp => {
+        console.log("Server result", resp);
+      });
+  }, []);
+  
   
   return (
     <>
