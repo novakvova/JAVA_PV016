b14647612b4ef504e702eb9fd6b95277782abdc1
novakvova <novakvova@gmail>
2023-02-22 11:28:03 +0200

    Begin workign files

--- shop/webapi/src/main/java/shop/controllers/HomeController.java
+++ shop/webapi/src/main/java/shop/controllers/HomeController.java
@@ -1,19 +1,26 @@
 package shop.controllers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import shop.dto.CategoryDTO;
+import shop.dto.UploadImageDto;
 import shop.repositories.CategoryRepository;
+import shop.storage.StorageProperties;
+import shop.storage.StorageService;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 public class HomeController {
-
+    private final StorageService storageService;
     private static List<CategoryDTO> list= new ArrayList<>();
     @GetMapping("/")
     public List<CategoryDTO> index() {
@@ -23,5 +30,22 @@ public class HomeController {
     @PostMapping("/")
     public void add(@RequestBody CategoryDTO category) {
         list.add(category);
+    }
+
+    @PostMapping("/upload")
+    public String upload(@RequestBody UploadImageDto dto) {
+        String fileName = storageService.save(dto.getBase64());
+        return fileName;
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serverfile(@PathVariable String filename) throws Exception {
+        Resource file = storageService.loadAsResource(filename);
+        String urlFileName = URLEncoder.encode("Сало.jpg", StandardCharsets.UTF_8.toString());
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "filename=\""+urlFileName+"\"")
+                .body(file);
     }
 }
---
+++ shop/webapi/src/main/java/shop/storage/StorageFileNotFoundException.java
@@ -0,0 +1,13 @@
+package shop.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
