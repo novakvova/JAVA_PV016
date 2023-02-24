374a9b9436dc1d50ecb213997fb20c192cc439f8
novakvova <novakvova@gmail>
2023-02-24 19:06:21 +0200

    Working show list categories

--- shop/my-app/src/components/home/index.tsx
+++ shop/my-app/src/components/home/index.tsx
@@ -1,38 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const callouts = [
-  {
-    name: 'Desk and Office',
-    description: 'Work from home accessories',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
-    imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
-    href: '#',
-  },
-  {
-    name: 'Self-Improvement',
-    description: 'Journals and note-taking',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
-    imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
-    href: '#',
-  },
-  {
-    name: 'Travel',
-    description: 'Daily commute essentials',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
-    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
-    href: '#',
-  },
-  {
-    name: 'Travel 2',
-    description: 'Daily commute essentials',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
-    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
-    href: '#',
-  },
-]
-
-
 
 interface ICategoryItem {
   id: number,
@@ -70,7 +38,7 @@ const Home = () => {
               <div key={category.id} className="group relative">
                 <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
                   <img
-                    src={category.image}
+                    src={"http://localhost:8083/files/"+category.image}
                     alt={category.name}
                     className="h-full w-full object-cover object-center"
                   />
