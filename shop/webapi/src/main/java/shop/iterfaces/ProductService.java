d98809a8f0e70d58b271c9bfb7cdd4495abe6298
novakvova <novakvova@gmail>
2023-03-10 18:58:05 +0200

    Working app react

--- shop/webapi/src/main/java/shop/iterfaces/ProductService.java
+++ shop/webapi/src/main/java/shop/iterfaces/ProductService.java
@@ -1,6 +1,7 @@
 package shop.iterfaces;
 
 import shop.dto.product.ProductCreateDTO;
+import shop.dto.product.ProductEditDTO;
 import shop.dto.product.ProductItemDTO;
 
 import java.util.List;
@@ -8,5 +9,8 @@ import java.util.List;
 public interface ProductService {
     ProductItemDTO create(ProductCreateDTO model);
     List<ProductItemDTO> get();
+    ProductItemDTO edit(int id, ProductEditDTO model);
+    ProductItemDTO getById(int id);
+
 }
 
